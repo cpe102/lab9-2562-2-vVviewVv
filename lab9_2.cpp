34819d44b8a8156d3ef510cc45a9b79f517859cd
vVviewVv <viewissara1246@gmail.com>
2020-01-28 16:40:12 +0700

    done

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,3 +2,22 @@
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
 //Don't forget to change cout to output filestream
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+    ifstream source;
+    source.open("C:\\view\\lab9-2562-2-vVviewVv\\cheerbook.txt");
+    string TT;
+    ofstream s1("C:\\view\\lab9-2562-2-vVviewVv\\cheerbook_copy.txt");
+    
+    s1 << "“-------------------- SOTUS ---------------------”\n";
+    while(getline(source,TT)){
+        s1 << TT << "\n";
+    }
+    s1 << "“-------------------- SOTUS ---------------------”\n";
+    source.close();
+    return 0;
+}
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,30 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int count = 0;
+    float mean = 0;
+    float sd = 0;
+    ifstream source("score.txt");
+    string text;
+    
+    while (getline(source,text)){
+        cout << text << "\n";
+        mean += atof(text.c_str());
+        sd += pow(atof(text.c_str()),2);
+        count++;
+    }
+    mean = mean/count;
+    sd = sqrt((sd/count)-pow(mean,2));
+    cout << "Number of data = " << count << "\n";
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << sd << "\n";
+
+}
